323463d45bb9d8aa371eebe4b811d81f4b4b8fc1
George Gelaga-King <gb7309@gmail.com>
2021-09-03 14:55:27 +0000

    Add: Machine resources

--- main.go
+++ main.go
@@ -89,6 +89,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "KINDCluster")
 		os.Exit(1)
 	}
+	if err = (&controllers.KINDMachineReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "KINDMachine")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
